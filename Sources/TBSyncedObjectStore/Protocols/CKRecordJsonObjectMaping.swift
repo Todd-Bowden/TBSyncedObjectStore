fbafc72d237ace308b53ab5811dd9348157723c9
Todd Bowden <toddhokie@gmail.com>
2022-11-19 13:12:03 -0500

    add CKRecordJsonObjectMaping

---
+++ Sources/TBSyncedObjectStore/Protocols/CKRecordJsonObjectMaping.swift
@@ -0,0 +1,33 @@
+//
+//  CKRecordJsonObjectMaping.swift
+//  
+//
+//  Created by Todd Bowden on 11/17/22.
+//
+
+import Foundation
+import CloudKit
+
+public class CKRecordJsonObjectMaping: CKRecordMapingProtocol {
+    
+    public func ckRecord(baseRecord: CKRecord, object: Codable) throws -> CKRecord {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .sortedKeys
+        let jsonObject = try encoder.encode(object).utf8string()
+        let record = baseRecord
+        record["object"] = jsonObject
+        return record
+    }
+    
+    public func object(ckRecord: CKRecord, type: Codable.Type?) throws -> Codable {
+        guard let json = ckRecord["object"] as? String else {
+            throw TBSyncedObjectStoreError.ckRecordObjectMissing
+        }
+        let decoder = JSONDecoder()
+        guard let type = type else {
+            throw TBSyncedObjectStoreError.codableObjectTypeNotProvided
+        }
+        return try decoder.decode(type, from: json.utf8data())
+    }
+    
+}
