d27e5afc31b346ffafb64acf22034707364f9ce4
Todd Bowden <toddhokie@gmail.com>
2022-11-19 13:11:43 -0500

    Use Codable object and mapping

--- Sources/TBSyncedObjectStore/Objects/SyncableObject.swift
+++ Sources/TBSyncedObjectStore/Objects/SyncableObject.swift
@@ -11,95 +11,114 @@ import CloudKit
 public struct SyncableObject {
     
     private struct Keys {
-        static let object = "object"
-        static let tombstone = "tombstone"
-        static let commit = "commit"
+        static let tombstone = "_tombstone"
+        static let commit = "_commit"
     }
     
     public let locator: ObjectLocator
-    public let objectJson: String
+    public let object: Codable?
     public let isTombstone: Bool
     public let archivedMetadata: Data?
     public var commit: ObjectCommit
+        
+    private let codableType: Codable.Type?
+    private let recordMaping: CKRecordMapingProtocol?
     
     public var id: String { locator.id }
     public var type: String { locator.type }
     public var user: String? { locator.user }
     public var isNotTombstone: Bool { !isTombstone }
     
-    private var ckMetadata: CKRecord? {
+    private var objectJson: String? {
+        guard let object = self.object else { return nil }
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .sortedKeys
+        return try? encoder.encode(object).utf8string()
+    }
+    
+    private var ckMetadataRecord: CKRecord? {
         guard let archivedMetadata = archivedMetadata else { return nil }
         return CKRecord(archivedMetadata: archivedMetadata)
     }
     
     public var metadata: Metadata? {
-        guard let ckMetadata = ckMetadata else { return nil }
+        guard let ckMetadata = ckMetadataRecord else { return nil }
         return Metadata(ckRecord: ckMetadata)
     }
     
-    public func object<T:Codable>() throws -> T {
-        guard let data = objectJson.data(using: .utf8) else {
-            throw TBSyncedObjectStoreError.utf8EncodingError
-        }
-        return try JSONDecoder().decode(T.self, from: data)
-    }
-    
-    public func object(type: Codable.Type) throws -> Codable {
-        return try JSONDecoder().decode(type, from: objectJson.utf8data())
+    private func object(json: String?, type: Codable.Type) -> Codable? {
+        guard let json = json else { return nil }
+        return try? JSONDecoder().decode(type, from: json.utf8data())
     }
     
     public func syncdata(staus: Syncdata.Status) -> Syncdata {
         Syncdata(locator: locator, status: staus, isTombstone: isTombstone, commit: commit)
     }
     
-    internal var ckRecord: CKRecord {
-        let record = ckMetadata ?? CKRecord(recordType: type, recordID: CKRecord.ID(recordName: id))
-        record[Keys.object] = isTombstone ? "" : objectJson
+    internal func ckRecord() throws -> CKRecord {
+        var record = ckMetadataRecord ?? CKRecord(recordType: type, recordID: CKRecord.ID(recordName: id))
+        guard isNotTombstone else {
+            record[Keys.tombstone] = true
+            record[Keys.commit] = commit.string
+            return record
+        }
+        guard let recordMaping = recordMaping else {
+            throw TBSyncedObjectStoreError.recordMapingNotProvided
+        }
+        if let object = self.object {
+            record = try recordMaping.ckRecord(baseRecord: record, object: object)
+        }
         record[Keys.tombstone] = isTombstone
         record[Keys.commit] = commit.string
         return record
     }
                                           
-    init(ckRecord: CKRecord, user: String?) {
+    init(ckRecord: CKRecord, type: Codable.Type, recordMaping: CKRecordMapingProtocol, user: String?) throws {
         self.locator = ObjectLocator(id: ckRecord.recordID.recordName, type: ckRecord.recordType, user: user)
-        self.objectJson = ckRecord[Keys.object] as? String ?? ""
         self.isTombstone = ckRecord[Keys.tombstone] as? Bool ?? false
         let commit = ckRecord[Keys.commit] as? String ?? ""
         self.commit = ObjectCommit(commit) ?? ObjectCommit.empty
         self.archivedMetadata = ckRecord.archivedMetadata
+        
+        if isTombstone {
+            self.codableType = nil
+            self.recordMaping = nil
+            self.object = nil
+        } else {
+            self.codableType = type
+            self.recordMaping = recordMaping
+            self.object = try recordMaping.object(ckRecord: ckRecord, type: type)
+        }
     }
     
     init(tombstoneId: String, type: String, user: String?, metadata: Data?, commit: ObjectCommit) {
         self.locator = ObjectLocator(id: tombstoneId, type: type, user: user)
-        self.objectJson = ""
+        self.object = nil
         self.isTombstone = true
-        self.commit = commit
+        self.commit = commit.tombstone
         self.archivedMetadata = metadata
+        self.recordMaping = nil
+        self.codableType = nil
     }
     
-    init(object: String, locator: ObjectLocator, metadata: Data?, commit: ObjectCommit) throws {
-        self.locator = locator
-        self.isTombstone = false
-        self.objectJson = object
-        self.archivedMetadata = metadata
-        self.commit = commit
-    }
-    
-    init(object: String, locator: ObjectLocator, metadata: Data?, syncdata: Syncdata) throws {
+    init(object: Codable, locator: ObjectLocator, metadata: Data?, syncdata: Syncdata, codableType: Codable.Type?, recordMaping: CKRecordMapingProtocol?) throws {
         self.locator = locator
         self.isTombstone = syncdata.isTombstone
-        self.objectJson = object
+        self.object = object
         self.archivedMetadata = metadata
         self.commit = syncdata.commit
+        self.codableType = codableType
+        self.recordMaping = recordMaping
     }
     
-    init(object: Codable, locator: ObjectLocator, metadata: Data?, commit: ObjectCommit) throws {
-        let encoder = JSONEncoder()
-        let data = try encoder.encode(object)
-        guard let objectJson = String(data: data, encoding: .utf8) else {
-            throw TBSyncedObjectStoreError.utf8EncodingError
-        }
-        self = try SyncableObject(object: objectJson, locator: locator, metadata: metadata, commit: commit)
+    init(object: Codable, locator: ObjectLocator, metadata: Data?, commit: ObjectCommit, codableType: Codable.Type?, recordMaping: CKRecordMapingProtocol?) throws {
+        self.locator = locator
+        self.isTombstone = commit.isTombstone
+        self.object = object
+        self.archivedMetadata = metadata
+        self.commit = commit
+        self.codableType = codableType
+        self.recordMaping = recordMaping
     }
     
 }
