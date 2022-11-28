5a55b7d0c0a6fc5458d233be56cf14e7f0a4c895
Todd Bowden <toddhokie@gmail.com>
2022-11-28 17:23:13 -0500

    use .product in target

--- Package.swift
+++ Package.swift
@@ -25,7 +25,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TBSyncedObjectStore",
-            dependencies: ["TBFileManager"]),
+            dependencies: [
+                .product(
+                    name: "TBFileManager",
+                    package: "TBFileManager")]
+        ),
         .testTarget(
             name: "TBSyncedObjectStoreTests",
             dependencies: ["TBSyncedObjectStore"]),
